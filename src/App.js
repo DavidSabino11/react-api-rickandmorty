d9806daa843f55fbcef8193f2a63c75e2f0bb192
David Sabino <david.sabino17@gmail.com>
2022-04-14 17:08:54 -0300

    Adição da barra de pesquisa e ajutes no visual do header

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import axios from "axios";
 
 function App() {
   const [characters, setCharacters] = useState([]);
+  const [nameChar, setNameChar] = useState("");
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const { data } = await axios.get(
-          `https://rickandmortyapi.com/api/character/`
+          `https://rickandmortyapi.com/api/character/?name=${nameChar}`
         );
         setCharacters(data.results);
       } catch (error) {
@@ -18,13 +19,22 @@ function App() {
     };
 
     fetchData();
-  }, []);
+  }, [nameChar]);
 
   return (
     <div className="App">
+      <div className="header">
+        <input
+          type="text"
+          placeholder={"Pesquisar personagem"}
+          onChange={event => setNameChar(event.target.value)}
+          value={nameChar}
+        ></input>
+      </div>
+
       <div className="res">
         {characters.map((character) => (
-          <div>
+          <div className="name">
             <img src={character.image} alt={character.name}></img>
             {character.name}
           </div>
