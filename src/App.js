4f22383c2f9171c937655905b03edf7a074f62a6
David Sabino <david.sabino17@gmail.com>
2022-04-14 15:26:37 -0300

    Adicionando o axios e puxando imagem/nome dos personagens

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { data } = await axios.get(
+          `https://rickandmortyapi.com/api/character/`
+        );
+        setCharacters(data.results);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <h1>Teste</h1>
+      <div className="results">
+        {characters.map((character) => (
+          <div>
+            <img src={character.image} alt={character.name}></img>
+            {character.name}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
